b903053323653722e8a36e6849b2c4d5ce1ec798
Mindaugas Urbontaitis <mindaugas@urbontaitis.lt>
2020-01-05 18:21:35 +0200

    twenty five minutes countdown

---
+++ components/FlipCounter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <no-ssr>
+      <flip-countdown :deadline="datetime" />
+    </no-ssr>
+  </div>
+</template>
+
+<script>
+import FlipCountdown from 'vue2-flip-countdown'
+import { format } from 'date-fns'
+
+export default {
+  components: { FlipCountdown },
+  props: {
+    minutes: { type: Number, required: true }
+  },
+  data() {
+    return {
+      date: new Date()
+    }
+  },
+  computed: {
+    datetime() {
+      const minutes = this.minutes * 60 * 1000
+      const minutesLater = this.date.getTime() + minutes
+      return format(new Date(minutesLater), 'yyyy-MM-dd HH:mm:ss')
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,23 @@
 <template>
   <div class="container">
     <div>
-      <logo />
       <h1 class="title">
-        flip-clock
+        Flip clock
       </h1>
       <h2 class="subtitle">
         Flip clock and pomodoro technique
       </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <flip-counter minutes="25" />
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import FlipCounter from '~/components/FlipCounter.vue'
 
 export default {
   components: {
-    Logo
+    FlipCounter
   }
 }
 </script>
